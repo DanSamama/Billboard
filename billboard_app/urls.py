48dac5bb3454b2cb01cd152fa43695167ae855a7
Benji <benjipollock@gmail.com>
2016-08-18 15:38:58 +0300

    billboard-6 Writing simple forms in HTML

--- billboard_app/urls.py
+++ billboard_app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'billboard_app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^billboard_app/new_msg', views.new_msg, name='new_msg'),
 ]
