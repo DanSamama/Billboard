ca7c0cca3f0e5f89031b59a7cd06c6239be78c0d
Benji <benjipollock@gmail.com>
2016-08-18 11:38:25 +0300

    billboard-4 Create models

--- billboard_app/models.py
+++ billboard_app/models.py
@@ -1,3 +1,20 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Message(models.Model):
+    pub_date = models.DateTimeField('date published')
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=500)
+    author = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    def posted_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
