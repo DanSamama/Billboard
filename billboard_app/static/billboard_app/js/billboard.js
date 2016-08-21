de9fe9789d0855c025b715f274ef8c765eea59ef
Benji <benjipollock@gmail.com>
2016-08-21 11:42:44 +0300

    Fixed red button functionality

--- billboard_app/static/billboard_app/js/billboard.js
+++ billboard_app/static/billboard_app/js/billboard.js
@@ -3,9 +3,15 @@ $('document').ready(function(){
     $('.submit_button').hide();
 
     $("#plus_button").click(function(){
-    $('.plus_square').hide()
-    $('.form').show()
+    $('.plus_square').hide();
+    $('.form').show();
         $('.submit_button').show();
+        });
+    $(".button_check_red") .click(function(){
+       $('.form').hide();
+       $('.submit_button').hide();
+       $('.plus_square').show();
+        $('#main_form').trigger("reset");
 });
 
 });
