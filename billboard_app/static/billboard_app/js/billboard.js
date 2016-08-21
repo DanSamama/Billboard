7787bdea48129c0bbb0cacd587caa1c7a327b22d
Benji <benjipollock@gmail.com>
2016-08-21 12:10:41 +0300

    Date shows up on form

--- billboard_app/static/billboard_app/js/billboard.js
+++ billboard_app/static/billboard_app/js/billboard.js
@@ -3,6 +3,9 @@ $('document').ready(function(){
     $('.submit_button').hide();
 
     $("#plus_button").click(function(){
+       var now = new Date($.now());
+        var time = (now.getMonth()+1) + '/' + now.getDate() + '/' + now.getFullYear();
+        $('#form_publication').text(time);
     $('.plus_square').hide();
     $('.form').show();
         $('.submit_button').show();
--- billboard_app/views.py
+++ billboard_app/views.py
@@ -11,6 +11,7 @@ from.models import Message
 def index(request):
     latest_message_list = Message.objects.order_by('-pub_date')[:5]
     context = {'latest_message_list': latest_message_list}
+    now = timezone.now()
     return render(request, 'billboard_app/index.html', context)
 
 
